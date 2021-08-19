a5a28d13ef469064c226d5d2fd3302fdf98ae270
Maria Grivorieva <masha_21.02.99@mail.ru>
2021-08-19 12:44:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            // создаем новый поток
+            Thread myThread = new Thread(new ParameterizedThreadStart(Count));
+            myThread.Start(number);
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count(object x)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                int n = (int)x;
+
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * n);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
